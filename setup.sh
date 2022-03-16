31a35451dfcffba453d2f56f7921d624f77cccd6
mt09torrent <73883123+mt09torrent@users.noreply.github.com>
2022-03-16 11:46:29 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mv frontend_start /usr/local/sbin
+chmod 777 /usr/local/sbin/frontend_start
+mv retroarch_rg350 /usr/local/bin
+chmod 777 /usr/local/bin/retroarch_rg350
+mv ReenableRetroarchBoot /usr/local/bin
+chmod 777 /usr/local/bin/ReenableRetroarchBoot
