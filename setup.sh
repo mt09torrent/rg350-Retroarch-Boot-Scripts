90b20642905bebda6386f9c9431dc15d4e263a9a
mt09torrent <73883123+mt09torrent@users.noreply.github.com>
2022-03-16 12:02:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 mv frontend_start /usr/local/sbin
 chmod 777 /usr/local/sbin/frontend_start
-mv retroarch_rg350 /usr/local/bin
-chmod 777 /usr/local/bin/retroarch_rg350
+
 mv ReenableRetroarchBoot /usr/local/bin
 chmod 777 /usr/local/bin/ReenableRetroarchBoot
+
+echo "don't miss to :
+mv retroarch_rg350 /usr/local/bin"
+chmod 777 /usr/local/bin/retroarch_rg350"
